f71b77aff7bb8f50548d0a36e719ca2d1eed9992
Erik Jensen <ejensen@netradius.com>
2021-01-17 23:56:02 -0700

    Adding additional outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "certificate_arn" {
 output "certificate_domain" {
   value = aws_acm_certificate.crt.domain_name
 }
+
+output "zone_id" {
+  value = data.aws_route53_zone.validation.zone_id
+}
+
+output "zone_name" {
+  value = data.aws_route53_zone.validation.name
+}
+
+output "certificate_validation_id" {
+  value = aws_acm_certificate_validation.crt.id
+}
